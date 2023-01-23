83dddb5fd0835b28fdf6457312fcf1e8dbdbdc81
CHARLES EDRISON VILLONES <charles08villones@gmail.com>
2023-01-23 15:59:21 +0800

    with sir mark

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,15 +1,78 @@
 import DisplayFeeds from "./displayFeeds";
 import DisplayPigs from "./displayPigs";
 import DisplayMedicines from "./displayMedicines";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import './Dashboard.css';
 
 
 const Dashboard =() => {
     return (
-        <div className="Dashboard-container">
-            <DisplayPigs></DisplayPigs>
+        <div style={{width:"1000px", backgroundColor:"white", justifyContent:"center", display:"flex"}} className="Dashboard-container">
+            {/* <DisplayPigs></DisplayPigs>
             <DisplayMedicines></DisplayMedicines>
-            <DisplayFeeds></DisplayFeeds>
+            <DisplayFeeds></DisplayFeeds> */}
+            <table class="table">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+
+<table class="table">
+  <thead class="thead-light">
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
         </div>
     )
 } 
